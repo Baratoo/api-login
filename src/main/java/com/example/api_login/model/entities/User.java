b372d01aed504c818d2b13247e514593bfcb7cc6
Baratoo <kaique_luiz1@hotmail.com>
2024-10-14 10:43:10 -0300

    criando user

---
+++ src/main/java/com/example/api_login/model/entities/User.java
@@ -0,0 +1,29 @@
+package com.example.api_login.model.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	private String id;
+	
+	private String nome;
+	private String email;
+	private String senha;
+}
